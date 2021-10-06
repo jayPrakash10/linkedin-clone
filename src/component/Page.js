fce585ada05083e83eb41d9f7edbdf4415ffbf2a
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-10-06 21:13:44 +0530

    Add files via upload

--- src/component/Page.js
+++ src/component/Page.js
@@ -6,6 +6,25 @@ function Page(){
         <div className='container'>
             <div className='content'>
                 <Profile/>
+                <div className='navigation'>
+                    <div>Overview</div>
+                    <div>About</div>
+                    <div>Products</div>
+                    <div>Post</div>
+                    <div>Jobs</div>
+                    <div>People</div>
+                    <div>Videos</div>
+                </div>
+                <div className='description'>
+                    <h3>
+                        About
+                    </h3>
+                    <p>Figma is the first professional-grade online tool created specifically for interface design. Born on the Web, Figma helps the entire product team create, test, and ship better designs, faster.</p>
+                </div>
+                
+            </div>
+            <div className='ad'>
+                People
             </div>
         </div>
     )
